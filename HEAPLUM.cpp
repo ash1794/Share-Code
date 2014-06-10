6a357ea6e4b3b00c876fa8aacb818eb8039dc2fb
Ashwin Krish <ash1794@gmail.com>
2014-06-10 13:07:34 +0530

    Struct

--- HEAPLUM.cpp
+++ HEAPLUM.cpp
@@ -2,9 +2,8 @@
 
 using namespace std;
 
-class Node
+struct Node
 {
-public:
     Node *left,*right;
     string label;
     int priority;
@@ -19,13 +18,12 @@ public:
             delete right;
     }
 };
-class treap
+struct treap
 {
-public:
-    Node   *roots;
+    struct Node   *roots;
     size_t tree_size;
 
-    treap():roots(NULL),tree_size(0)
+    treap():tree_size(0)
     {
     }
     ~treap()
