2257793563b50f4a84d726f6a4cd80e4dd76e189
Ashwin Krish <ash1794@gmail.com>
2014-06-10 12:56:31 +0530

    Spoj HEAPLUM

---
+++ HEAPLUM.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    Node *left,*right;
+    string label;
+    int priority;
+
+    Node():left(NULL),right(NULL),label(NULL),priority(0)
+    {}
+    ~Node()
+    {
+        if(left)
+            delete left;
+        if(right)
+            delete right;
+    }
+};
+class treap
+{
+public:
+    Node   *roots;
+    size_t tree_size;
+
+    treap():roots(NULL),tree_size(0)
+    {
+    }
+    ~treap()
+    {
+        if(roots)
+            delete roots;
+    }
+    void insert(string label,int pri)
+    {
+        Node *temp = new Node();
+        temp->label=label;
+        temp->priority=pri;
+        if(tree_size==0)
+        {
+            roots = temp;
+            tree_size++;
+            return;
+        }
+
+        Node *where = roots,*prev = NULL;
+        while(where!=NULL and where->priority > temp->priority)
+        {
+            prev = where;
+            if(temp->label < where->label)
+                where = where->left;
+            else
+                where = where->right;
+        }
+        if(where==NULL)
+        {
+            if(temp->label < prev->label)
+                prev->left = temp;
+            else
+                prev->right = temp;
+        }
+        else
+        {
+            if(temp->label < prev->label)
+                prev->left = temp;
+            else
+                prev->right = temp;
+            if(where->label < temp->label)
+                temp->left = where;
+            else
+                temp->right = where;
+        }
+    }
+
+    void print(Node *trav = roots)
+    {
+        if(trav == NULL)
+            return;
+        cout<<"(";
+        print(trav->left);
+        cout<<trav->label<<'/'<<trav->priority;
+        print(trav->right);
+        cout<<")";
+    }
+};
+int main()
+{
+    return 0;
+}
