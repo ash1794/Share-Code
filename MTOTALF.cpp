7819bb7e9bdee00067c05298e525088d61fe8864
Ashwin Krish <ash1794@gmail.com>
2014-06-19 14:13:13 +0530

    Edmond Karp Example

--- MTOTALF.cpp
+++ MTOTALF.cpp
@@ -5,11 +5,10 @@ using namespace std;
 int graph[60][60];
 int rgraph[60][60];
 
-const bool debug = false;
-int ford(int maxv,int s,int t)
+int ford(int s,int t)
 {
-    for(int i=1;i<maxv;i++)
-        for(int j=1;j<maxv;j++)
+    for(int i=0;i<52;i++)
+        for(int j=0;j<52;j++)
             rgraph[i][j]=graph[i][j];
 
     int max_flow = 0;
@@ -18,21 +17,17 @@ int ford(int maxv,int s,int t)
 
     while(path_exists)
     {
-        
-
         queue<int> q;
-        vector<int> par(60,-1);
-        vector<int> visited(60,0);
-        q.push(s);
-        visited[s]=1;
+        vector<int> par(55,-1);
+        vector<int> visited(55,0);
+        q.push(0);
+        visited[0]=1;
         while(!q.empty())
         {
             int cur = q.front();q.pop();
-
-            if(debug)cout<<cur<<" ";
             if(cur==t)
                 break;
-            for(int i=0;i<maxv;i++)
+            for(int i=0;i<52;i++)
             {
                 if(visited[i]==false and rgraph[cur][i] > 0)
                 {
@@ -42,7 +37,6 @@ int ford(int maxv,int s,int t)
                 }
             }
         }
-        if(debug)cout<<"\n";
         if(!visited[t])
         {
             path_exists=false;
@@ -51,40 +45,34 @@ int ford(int maxv,int s,int t)
         int path_flow = INT_MAX;
         for(int v = t; v!=s ; v=par[v])
         {
-            if(debug)cout<<v<<" ";
             path_flow = min(path_flow,rgraph[par[v]][v]);
         }
-        if(debug)cout<<": "<<path_flow<<" ";
         for(int v = t; v!=s ; v=par[v])
         {
             rgraph[par[v]][v] -= path_flow;
             rgraph[v][par[v]] += path_flow;
         }
-        if(debug)cout<<", Max_Flow = "<<max_flow<<"\n";
         max_flow += path_flow;
     }
     return max_flow;
 }
-
+int nodeid(char ch)
+{
+    if(ch>='a' and ch<='z')
+        return ch-'a'+26;
+    return ch-'A';
+}
 int main()
 {
-    //cout<<nodeid('Z')<<" "<<nodeid('a')<<" "<<nodeid('z')<<"\n";
     int e;
     cin>>e;
     char sr,ds;
     int flow;
-    int nodeid = 1;
-    map<char,int> nodemap;
     for(int i=0;i<e;i++)
     {
         cin>>sr>>ds>>flow;
-        if(!nodemap[sr])
-            nodemap[sr]=nodeid++;
-        if(!nodemap[ds])
-            nodemap[ds]=nodeid++;
-
-        graph[nodemap[sr]][nodemap[ds]]=flow;
+        graph[nodeid(sr)][nodeid(ds)]+=flow;
     }
-    cout<<ford(nodeid,nodemap['A'],nodemap['Z'])<<"\n";
+    cout<<ford(nodeid('A'),nodeid('Z'))<<"\n";
     return 0;
 }
